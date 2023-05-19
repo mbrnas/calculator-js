94cfb0fccac0eb9a07fa6ab8b944d666faad6a21
Matija Brnas <mb8828@rit.edu>
2023-05-20 01:28:23 +0200

    print numbers to display

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 // get all elements from html
+// import { log } from "console";
 import {
   addNumbers,
   subtractNumbers,
   divideNumbers,
   multiplyNumbers,
-} from "./calculatorMath";
+} from "./calculatorMath.js";
 
+const display = document.querySelector(".display");
+const digit = document.querySelectorAll(".digit");
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const multiply = document.getElementById("multiply");
+const clearDisplay = document.getElementById("clear-display");
+const equals = document.getElementById("equals");
+const divide = document.getElementById("divide");
+
+
+digit.forEach(function (digit) { 
+    digit.addEventListener("click", function () { 
+        display.textContent += digit.innerText;
+     });
+ });
+
+ 

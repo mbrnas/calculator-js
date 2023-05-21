64d87399b2673a8ff55d37804f9b6a006b98726a
Matija Brnas <mb8828@rit.edu>
2023-05-21 23:49:44 +0200

    deleted mathLogic file

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-// get all elements from html
-// import { log } from "console";
-import {
-  addNumbers,
-  subtractNumbers,
-  divideNumbers,
-  multiplyNumbers,
-} from "./calculatorMath.js";
 
 const display = document.querySelector(".display");
 const digit = document.querySelectorAll(".digit");
@@ -15,9 +7,3 @@ const multiply = document.getElementById("multiply");
 const clearDisplay = document.getElementById("clear-display");
 const equals = document.getElementById("equals");
 const divide = document.getElementById("divide");
-
-digit.forEach(function (digit) {
-  digit.addEventListener("click", function () {
-    display.textContent += digit.innerText;
-  });
-});

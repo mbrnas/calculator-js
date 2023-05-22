11644f4edb170f84a0ac3c0f1264525e54bdb5c0
Matija Brnas <mb8828@rit.edu>
2023-05-22 11:11:02 +0200

    finished the project in a sitting, finishing styling

--- js/script.js
+++ js/script.js
@@ -1,9 +1,36 @@
+let display = document.querySelector(".display");
+let digit = document.querySelectorAll(".digit");
+let deleteButton = document.getElementById("delete");
+let clearDisplay = document.getElementById("clear-display");
+let equals = document.getElementById("equals");
 
-const display = document.querySelector(".display");
-const digit = document.querySelectorAll(".digit");
-const add = document.getElementById("add");
-const subtract = document.getElementById("subtract");
-const multiply = document.getElementById("multiply");
-const clearDisplay = document.getElementById("clear-display");
-const equals = document.getElementById("equals");
-const divide = document.getElementById("divide");
+
+
+digit.forEach(function (digit) { 
+    digit.addEventListener("click", function () { 
+        display.textContent += digit.innerText;
+     });
+ });
+
+clearDisplay.addEventListener("click", function () {
+  display.textContent = "";
+});
+
+equals.addEventListener("click", function () {
+    if (display.textContent == "") {
+      display.textContent = "";
+    } else {
+      let answer = eval(display.textContent);
+      display.textContent = answer;
+    }
+});
+
+
+deleteButton.addEventListener("click", function(){
+    if (display.textContent == "") {
+        display.textContent = "";
+      }
+      else{
+        display.textContent = display.textContent.slice(0, -1);
+      }
+});
